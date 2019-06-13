e30502568f28380f1a5eb355d38590f403478a10
Brandon A <br.araki@gmail.com>
2019-06-12 20:09:39 -0400

    added some comments

--- convolution_lstm.py
+++ convolution_lstm.py
@@ -1,7 +1,16 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import pdb
 
+''' NOTES
+- steps are how many times you recurse over the data! (eg the length of the trajectory)
+- effective_steps is a list of which step outputs you want to save
+- the format of the data as-is is [batch size, input channels, h, w, d]
+- you need to change it to accept [batch size, trajectory, input channels, h, w, d]
+  and then have 'step' index the trajectory
+
+'''
 
 class ConvLSTMCell(nn.Module):
     def __init__(self, input_channels, hidden_channels, kernel_size):
@@ -48,11 +57,10 @@ class ConvLSTMCell(nn.Module):
         return (Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda(),
                 Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda())
 
-
 class ConvLSTM(nn.Module):
     # input_channels corresponds to the first input feature map
     # hidden state is a list of succeeding lstm layers.
-    def __init__(self, input_channels, hidden_channels, kernel_size, step=1, effective_step=[1]):
+    def __init__(self, input_channels, hidden_channels, kernel_size, step=1, effective_step=[0]):
         super(ConvLSTM, self).__init__()
         self.input_channels = [input_channels] + hidden_channels
         self.hidden_channels = hidden_channels
@@ -91,6 +99,104 @@ class ConvLSTM(nn.Module):
 
         return outputs, (x, new_c)
 
+class ConvLSTMCell3d(nn.Module):
+    def __init__(self, input_channels, hidden_channels, kernel_size):
+        super(ConvLSTMCell3d, self).__init__()
+
+        assert hidden_channels % 2 == 0
+
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+        self.num_features = 4
+
+        self.padding = 0 # int((kernel_size - 1) / 2)
+
+        self.Wxi = nn.Conv3d(in_channels=self.input_channels,
+                             out_channels=self.hidden_channels,
+                             kernel_size=self.kernel_size,
+                             stride=1, padding=self.padding,
+                             bias=True)
+        self.Whi = nn.Conv3d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+        self.Wxf = nn.Conv3d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.Whf = nn.Conv3d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+        self.Wxc = nn.Conv3d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.Whc = nn.Conv3d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+        self.Wxo = nn.Conv3d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.Who = nn.Conv3d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+
+        self.Wci = None
+        self.Wcf = None
+        self.Wco = None
+
+    def forward(self, x, h, c):
+        ci = torch.sigmoid(self.Wxi(x) + self.Whi(h) + c * self.Wci)
+        cf = torch.sigmoid(self.Wxf(x) + self.Whf(h) + c * self.Wcf)
+        cc = cf * c + ci * torch.tanh(self.Wxc(x) + self.Whc(h))
+        co = torch.sigmoid(self.Wxo(x) + self.Who(h) + cc * self.Wco)
+        ch = co * torch.tanh(cc)
+        return ch, cc
+
+    def init_hidden(self, batch_size, hidden, shape):
+        if self.Wci is None:
+            self.Wci = Variable(torch.zeros(1, hidden, shape[0], shape[1], shape[2])).cuda()
+            self.Wcf = Variable(torch.zeros(1, hidden, shape[0], shape[1], shape[2])).cuda()
+            self.Wco = Variable(torch.zeros(1, hidden, shape[0], shape[1], shape[2])).cuda()
+        else:
+            assert shape[0] == self.Wci.size()[2], 'Input Height Mismatched!'
+            assert shape[1] == self.Wci.size()[3], 'Input Width Mismatched!'
+            assert shape[2] == self.Wci.size()[4], 'Input Depth Mismatched!'
+        return (Variable(torch.zeros(batch_size, hidden, shape[0], shape[1], shape[2])).cuda(),
+                Variable(torch.zeros(batch_size, hidden, shape[0], shape[1], shape[2])).cuda())
+
+# CHANGES from the original code:
+# step now indexes the trajectory
+# got rid of effective_step because I need to save every step
+# the 3D conv needs only 1 input channel so I got rid of the
+# requirement you need an even # of input channels (what's up with that anyway?)
+
+class ConvLSTM3d(nn.Module):
+    # input_channels corresponds to the first input feature map
+    # hidden state is a list of succeeding lstm layers.
+    def __init__(self, input_channels, hidden_channels, kernel_size, step=1, effective_step=[0]):
+        super(ConvLSTM3d, self).__init__()
+        self.input_channels = [input_channels] + hidden_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+        self.num_layers = len(hidden_channels)
+        self.step = step
+        self.effective_step = effective_step
+        self._all_layers = []
+        for i in range(self.num_layers):
+            name = 'cell{}'.format(i)
+            cell = ConvLSTMCell3d(self.input_channels[i], self.hidden_channels[i], self.kernel_size)
+            setattr(self, name, cell)
+            self._all_layers.append(cell)
+
+    def forward(self, input):
+        internal_state = []
+        outputs = []
+        for step in range(self.step):
+            x = input
+            for i in range(self.num_layers):
+                # all cells are initialized in the first step
+                name = 'cell{}'.format(i)
+                if step == 0:
+                    bsize, _, height, width, depth = x.size()
+                    (h, c) = getattr(self, name).init_hidden(batch_size=bsize,
+                                                             hidden=self.hidden_channels[i],
+                                                             shape=(height, width, depth))
+                    internal_state.append((h, c))
+
+                # do forward
+                (h, c) = internal_state[i]
+                x, new_c = getattr(self, name)(x, h, c)
+                internal_state[i] = (x, new_c)
+            # only record effective steps
+            if step in self.effective_step:
+                outputs.append(x)
+
+        return outputs, (x, new_c)
 
 if __name__ == '__main__':
     # gradient check
@@ -102,6 +208,8 @@ if __name__ == '__main__':
     target = Variable(torch.randn(1, 32, 64, 32)).double().cuda()
 
     output = convlstm(input)
-    output = output[0][0].double()
-    res = torch.autograd.gradcheck(loss_fn, (output, target), eps=1e-6, raise_exception=True)
+    output2 = output[0][0].double()
+    res = torch.autograd.gradcheck(loss_fn, (output2, target), eps=1e-6, raise_exception=True)
     print(res)
+    pdb.set_trace()
+    print('hi')
